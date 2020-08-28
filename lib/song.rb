0b48b86f86609fce03c0e278e038c3c5c988478b
Aaron Brady <aleebrady@gmail.com>
2020-08-28 18:02:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -56,8 +56,8 @@ class Song
 
   def self.find_by_name(name)
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
-    DB[:conn].execute(sql)
     binding.pry
+    DB[:conn].execute(sql)
   end
 
 end
